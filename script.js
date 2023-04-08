01e9a1ded55ebe57c44f7cc1680714c547c03035
Ankit kumar <94157882+ankit0218@users.noreply.github.com>
2023-04-08 17:50:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function increment(){
+    let number1= document.getElementById("num1").value;
+    number1++;
+    document.getElementById('num1').value= number1;
+
+}
+
+
+function reset(){
+    let number2 = document.getElementById("num2").value;
+    number2=0;
+    document.getElementById('num2').value= number2;
+}
